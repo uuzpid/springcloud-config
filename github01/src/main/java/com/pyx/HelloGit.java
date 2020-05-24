78a68409283fca148d14c3750ecf457ca5589c33
tom_glb <goodMorning_glb@atguigu.com>
2020-05-24 17:51:26 +0800

    测试SSH

--- github01/src/main/java/com/pyx/HelloGit.java
+++ github01/src/main/java/com/pyx/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
         System.out.println("解决冲突2");
         System.out.println("在线更新");
         System.out.println("克隆项目改1");
+        System.out.println("测试SSH");
     }
 }
